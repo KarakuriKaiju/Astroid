6d5dbcabd69c33b3c4d189353d2008a61e99ff21
KarakuriKaiju <caseyanders1995@gmail.com>
2025-04-13 16:59:15 -0400

    added circleshape.py; provided by boots.dev

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
 if __name__ == "__main__":
     main()
